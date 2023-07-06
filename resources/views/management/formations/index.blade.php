a84aa3e07cb284f1a707d855dc7f532b4e0894aa
c-salhab <118256379+c-salhab@users.noreply.github.com>
2023-07-06 10:12:07 +0200

    Update index.blade.php

--- resources/views/management/formations/index.blade.php
+++ resources/views/management/formations/index.blade.php
@@ -68,7 +68,7 @@
             @foreach($formations as $formation)
                 <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        {{ $formation->creator_name }}
+                        {{ $formation->creator->name }}
                     </th>
 
                     <td class="px-6 py-4">

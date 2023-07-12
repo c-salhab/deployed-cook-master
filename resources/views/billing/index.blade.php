a72129912f21c0169008aebbe4f86dda04400db2
Teddy <truongteddy306@icloud.com>
2023-07-11 23:32:24 -0400

    billing panel modified

--- resources/views/billing/index.blade.php
+++ resources/views/billing/index.blade.php
@@ -2,7 +2,8 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <p class="text-2xl">
             Hello, <div class="text-indigo-600 text-3xl"><?php echo ucfirst(Auth::user()->first_name); ?></div>
-        <br><i>Welcome to the billing panel. Here you can manage your billing information from to shipping address.</i>
+        <br><i>Welcome to the billing panel. Here you can manage your billing information from your payment option to shipping address.</i>
+        <br><div class="text-indigo-600">You can also check your invoices and download them.</div>
         </p>
 
     </div>

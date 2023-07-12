c21fd8336ad78f92f47005e0880f67490cd63a75
Teddy <truongteddy306@icloud.com>
2023-07-12 08:07:01 -0400

    preview finished

---
+++ app/Http/Livewire/Lessons/LessonPreview.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Http\Livewire\Lessons;
+
+use App\Models\Lessons;
+use App\Models\LessonStep;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Livewire\Component;
+
+class LessonPreview extends Component
+{
+
+    public $lesson_id;
+    public $duration = 0;
+
+    public function mount($lesson_id){
+        $this->lesson_id = $lesson_id;
+    }
+
+    public function addCart(){
+        DB::table('carts')->insert([
+            'user_id' => auth()->user()->id,
+            'lesson_id' => $this->lesson_id,]);
+    }
+
+    public function removeCart($id){
+
+        DB::table('carts')->where('lesson_id', $this->lesson_id)->delete();
+    }
+    public function render()
+    {
+        $this->duration = 0;
+        $lesson = Lessons::where('id', $this->lesson_id)->first();
+        if(DB::table('carts')->where('lesson_id', $this->lesson_id)->first()){
+            $lesson['active'] = true;
+        }else{
+            $lesson['active'] = false;
+        };
+        $lesson_steps = LessonStep::where('lesson_id', $this->lesson_id)->get();
+        foreach ($lesson_steps as $step){
+            $this->duration += $step->duration;
+        }
+        $creator = User::where('id', $lesson->creator_id)->first();
+        return view('lessons.preview', ['lesson' => $lesson, 'duration'=> $this->duration, 'creator'=> $creator])->layout('layouts.app');
+    }
+}
